31d18d90393ccbf528b076817fef6427e5054243
Abhishekprojects <guptaabhishek44@yahoo.com>
2020-01-15 23:33:40 +0530

    Changes with navbar

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./header";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <h1>This is done </h1>
+    </div>
+  );
+};
+
+export default App;
